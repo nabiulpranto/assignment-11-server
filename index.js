e7325052537740b20622b3e65722ec2952671eb1
nabiulpranto <nabiulislampranto19@gmail.com>
2022-11-22 11:57:05 +0600

    error problem solved

--- index.js
+++ index.js
@@ -1,19 +1,48 @@
-const express = require("express");
-const cors = require("cors");
-const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-const app = express();
-const port = process.env.PORT || 7000;
-require("dotenv").config();
+const express = require('express')
+const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 
-// middleware
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middle wares
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.t8uwwnp.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
-const client = new MongoClient(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    serverApi: ServerApiVersion.v1,
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.swijrrg.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+  try{
+const serviceCollection = client.db('juiceBar').collection('services');
+
+app.get('/services', async(req,res) =>{
+  const query = {}
+  const cursor = serviceCollection.find(query);
+  const services = await cursor.toArray();
+  res.send(services);
+})
+  }
+finally{
+
+}
+}
+
+run().catch(err => console.error(err));
+
+
+app.get('/', (req, res) =>{
+    res.send('Juice Bar Server Running')
 });
-//
+
+
+
+app.listen(port, () => {
+    console.log(`Juice Bar Server is Running on port ${port} `);
+   
+})
